fdb07b27a4df562e5195e10ab1780cb0b863c114
Hidran Arias <hidran.arias@spryker.com>
2022-09-16 19:06:41 +0200

    Creates product social widget

--- src/Pyz/Yves/ProductSocialWidget/Widget/ProductSocialWidget.php
+++ src/Pyz/Yves/ProductSocialWidget/Widget/ProductSocialWidget.php
@@ -2,38 +2,10 @@
 
 namespace Pyz\Yves\ProductSocialWidget\Widget;
 
-
-use Generated\Shared\Transfer\ProductViewTransfer;
 use Spryker\Yves\Kernel\Widget\AbstractWidget;
 
 class ProductSocialWidget extends AbstractWidget
 {
-    protected const PARAMETER_PRODUCT = 'product';
-    protected const PARAMETER_SOCIAL_ICONS = 'socialIcons';
-    /**
-     * @param Generated\Shared\Transfer\ProductViewTransfer
-     */
-    public function __construct(ProductViewTransfer $product, array $socialIcons = [])
-{
-    $this->addProductParameter($product);
-    $this->addWidgets($socialIcons);
-    }
-
-    /**
-     * @return void
-     */
-
-
-    protected function addProductParameter(ProductViewTransfer $product): void
-
-    {
-        $this->addParameter(static::PARAMETER_PRODUCT, $product);
-    }
-
-    protected function addWidgets(array $socialIcons)
-    {
-        $this->addParameter(static::PARAMETER_SOCIAL_ICONS, $socialIcons);
-    }
 
     public static function getName(): string
     {
@@ -42,6 +14,6 @@ class ProductSocialWidget extends AbstractWidget
 
     public static function getTemplate(): string
     {
-       return '@ProductSocialWidget/views/product-social-widget/product-social-widget.twig';
+        return '@ProductSocialWidget/views/product-social-widget/product-social-widget.twig';
     }
 }
