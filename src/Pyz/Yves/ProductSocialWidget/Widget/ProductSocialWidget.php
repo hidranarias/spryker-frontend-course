83bbf1eb2aa56bac897d829e811b1f93fd9d703a
Hidran Arias <hidran.arias@spryker.com>
2022-09-19 00:06:40 +0200

    Adds product and socialIcons args to the widget

--- src/Pyz/Yves/ProductSocialWidget/Widget/ProductSocialWidget.php
+++ src/Pyz/Yves/ProductSocialWidget/Widget/ProductSocialWidget.php
@@ -2,11 +2,22 @@
 
 namespace Pyz\Yves\ProductSocialWidget\Widget;
 
+use Generated\Shared\Transfer\ProductViewTransfer;
 use Spryker\Yves\Kernel\Widget\AbstractWidget;
 
 class ProductSocialWidget extends AbstractWidget
 {
-
+ protected const PARAMETER_PRODUCT = 'product';
+ protected const PARAMETER_SOCIALICONS = 'socialIcons';
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $product
+     * @param array<string> $socialIcons
+     */
+    public function __construct(ProductViewTransfer $product, array $socialIcons)
+     {
+         $this->addProductParameter($product);
+         $this->addSocialIconsParameter($socialIcons);
+     }
     public static function getName(): string
     {
         return 'ProductSocialWidget';
@@ -15,5 +26,25 @@ class ProductSocialWidget extends AbstractWidget
     public static function getTemplate(): string
     {
         return '@ProductSocialWidget/views/product-social-widget/product-social-widget.twig';
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $product
+     *
+     * @return void
+     */
+    public function addProductParameter(ProductViewTransfer $product): void
+    {
+        $this->addParameter(static::PARAMETER_PRODUCT, $product);
+    }
+
+    /**
+     * @param array $socialIcons
+     *
+     * @return void
+     */
+    public function addSocialIconsParameter(array $socialIcons): void
+    {
+        $this->addParameter(static::PARAMETER_SOCIALICONS, $socialIcons);
     }
 }
